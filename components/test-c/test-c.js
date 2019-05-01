2bdbde401da12905e05248dbb517e9976ce64288
robinzevallos <zevallosrobin@gmail.com>
2019-05-01 11:02:11 -0500

    Added polymer

---
+++ components/test-c/test-c.js
@@ -0,0 +1,24 @@
+﻿Polymer({
+    is: 'test-c',
+
+    properties: {
+        tagi: String,
+        value2: String,
+        value: {
+            type: String,
+            reflectToAttribute: true,
+            notify: true,
+            value: 1
+        }
+    },
+
+    handleClick: function (e, detail) {
+        this.fire('kick', { kicked: true });
+    },
+
+    regularTap: function (e) {
+        alert("Thank you for tapping");
+    },
+});
+
+
